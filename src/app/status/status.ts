b9575d875938bab1d8533e17ee4c976b2be80403
Chawadol <chawadol2547@gmail.com>
2025-12-22 13:35:45 +0700

    update last

--- src/app/status/status.ts
+++ src/app/status/status.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-status',
@@ -7,6 +7,27 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   styleUrl: './status.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class Status {
+export class Status implements OnInit, OnDestroy {
+  currentTime: Date = new Date();
+  private timer!: number;
+  userAgent!: string;
+  languages!: string;
+  cookieEnabled!: boolean;
+  logicalProcessors!: number;
+  maxTouchPoints!: number;
 
+  ngOnInit() {
+    this.timer = setInterval(() => {
+      this.currentTime = new Date();
+    }, 1000);
+    this.userAgent = navigator.userAgent;
+    this.languages = navigator.language;
+    this.cookieEnabled = navigator.cookieEnabled;
+    this.logicalProcessors = navigator.hardwareConcurrency;
+    this.maxTouchPoints = navigator.maxTouchPoints;
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.timer);
+  }
 }
